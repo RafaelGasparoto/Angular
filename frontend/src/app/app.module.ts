1b5a6f798deb67bb9444f3b05e0a4fb9e6c8c065
Rafael <rafaelgasparoto4@gmail.com>
2022-09-21 23:55:19 -0300

    home adicionada e arrumei a sidenav que estava sumindo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,16 +11,21 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'; 
 
 import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list'
+
+import { MatCardModule } from '@angular/material/card'
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
+    MatCardModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
